6242db790ff116259274ebeed80ca5cfa90434e9
rashmichoudhary13 <rashmichoudhary019@gmail.com>
2026-03-30 22:33:07 +0530

    connected with TiDB sql database.

--- config/sql.js
+++ config/sql.js
@@ -1,13 +1,17 @@
 import mysql from "mysql2/promise";
 import 'dotenv/config';
-
+import fs from 'fs';
 // Connect with the server
 
 const db = await mysql.createConnection({
   host: process.env.SQL_HOST,
+  port: process.env.DB_PORT,
   user: process.env.SQL_USER,
   password: process.env.SQL_PASS,
   database: process.env.SQL_DATABASE,
+  ssl: {
+    ca: fs.readFileSync(process.env.CA)
+  }
 });
 
 console.log("MySQL Database connected successfully");
